bed56017916dda318338e7c934ed0d62d31df0a4
Earnest <earnestcpy@gmail.com>
2019-07-18 16:21:53 -0700

    Add lane_grayscale.py

--- Computer_vision/lane.py
+++ Computer_vision/lane.py
@@ -1,3 +1,8 @@
 import cv2
 
-print(cv2.__version__)
+#read image by calling cv2.imread()
+load_image = cv2.imread("./Image/Image/test_image.jpg")
+cv2.imshow("result", load_image)
+cv2.waitKey(0)
+
+
---
+++ Computer_vision/lane_grayscale.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+#read image by calling cv2.imread()
+load_image = cv2.imread("./Image/Image/test_image.jpg")
+copy_image = np.copy(load_image)
+
+#convert a RGB image to grayscale
+g_image = cv2.cvtColor(copy_image,cv2.COLOR_RGB2GRAY)
+
+
+cv2.imshow("result", g_image)
+cv2.waitKey(0)
+
+
